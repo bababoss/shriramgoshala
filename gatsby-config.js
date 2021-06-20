fa365ed726aec8d516e22a47729a4607359aed86
vikashkumarsaini1598 <vickykartoos1598@gmail.com>
2021-06-20 19:34:33 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  window: {
-    globalObject: `this`,
-  },
+
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
