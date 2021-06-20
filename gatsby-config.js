79436c9e2f15cf972b4e04040cc4f1d4d1252840
vikashkumarsaini1598 <vickykartoos1598@gmail.com>
2021-06-20 19:48:28 +0530

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,17 +33,5 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
+
 
